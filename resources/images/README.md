42b1a1dfdc6ce47c44065093cba774c29d82c812
Edwin <60476129+Edwinexd@users.noreply.github.com>
2024-02-04 12:27:44 +0100

    License under GPL V3.0 (#14)

---
+++ resources/images/README.md
@@ -0,0 +1,12 @@
+# Note
+None of the files in this directory fall under the LICENSE file in the root directory. They are all made by other people and are not our own work. We have only used them for educational purposes and have not made any money off of them.
+
+# Credits
+castle_full_health.png, castle_low_health.png, castle_medium_health.png, dalle_generated_bg.jpg:
+Made by [Edwin](https://github.com/Edwinexd)
+
+player_alive.png, player_dead.png:
+Made by [Space invaders icons created by Smashicons - Flaticon](https://www.flaticon.com/free-icons/space-invaders)
+
+alive.png, dead.png:
+Made by [Beguima](https://www.iconfinder.com/icons/1735038/invader_space_space_invader_icon)
--- src/KeyEventCallback.cpp
+++ src/KeyEventCallback.cpp
@@ -1,3 +1,20 @@
+/* 
+dsv-cprog-space-invaders - A space invaders inspired shooter written in C++ with SDL2
+Copyright (C) 2024 Edwin Sundberg and Erik Lind Gou-Said
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 #include <KeyEventCallback.h>
 
 KeyEventCallback::KeyEventCallback(std::string n_key, std::function<void(std::string, KeyPressType, Component &)> n_cfn, Component &n_comp) : assigned_key(std::vector<std::string>()), callback_fn(std::move(n_cfn)), target_comp(&n_comp)
