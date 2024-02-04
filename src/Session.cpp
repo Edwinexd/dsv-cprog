e17f1c083fd4643395e1e0ce3d0e6079218f5a94
Edwin <60476129+Edwinexd@users.noreply.github.com>
2024-02-04 12:26:46 +0100

    License under GPL V3.0

--- src/Session.cpp
+++ src/Session.cpp
@@ -1,3 +1,20 @@
+/* 
+dsv-cprog-space-invaders - A space invaders inspired shooter written in C++ with SDL2
+Copyright (C) 2024 Edwin Sundberg and Erik Lind Gou-Said
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 #include <SDL2/SDL.h>
 #include <stdexcept>
 #include <Session.h>
